d5978998e0e8479de3c1bb9661c4e03567c3c7e4
eth9n-dev <eckert6@gmail.com>
2024-04-22 11:46:28 -0700

    basic model page

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { db } from "~/server/db";
+import { getServerAuthSession } from "~/server/auth";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-type Props = {}
+type Props = {};
 
-const page = (props: Props) => {
+const Models = async (props: Props) => {
+  const session = await getServerAuthSession();
+  const prisma = new PrismaClient();
+
+  // Select all models with userId
+  // Format models into list of buttons
+
+  const models: unknown = await prisma.models.findMany({
+    where: {
+      userId: {
+        equals: session?.user.id,
+      },
+    },
+  });
+
   return (
-    <div>nsafjndfkjsdbnfsdkj</div>
-  )
-}
+    <div className="flex h-screen items-center justify-center">
+      <div className="rounded-lg border-2 border-black p-4">
+        <div className="flex flex-col items-center">
+          {models.map((model) => (
+            <div className="p-2">
+              <Button asChild>
+                <Link href={"/models/editor/" + model["id"]}>
+                  {" "}
+                  {model["name"]}{" "}
+                </Link>
+              </Button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Models;
