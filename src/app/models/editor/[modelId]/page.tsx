224cfbf8f6de2a1b99471036264767088fa743bc
eth9n-dev <eckert6@gmail.com>
2024-04-23 11:31:10 -0700

    ReactFlow: initial setup

--- src/app/models/editor/[modelId]/page.tsx
+++ src/app/models/editor/[modelId]/page.tsx
@@ -1,15 +1,58 @@
-import React from 'react'
+"use client";
+import { useState, useCallback } from 'react';
+import ReactFlow, {
+  Controls,
+  Background,
+  applyNodeChanges,
+  applyEdgeChanges,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
 
-type Props = {
-  params: { modelId: string }
-}
+const initialNodes = [
+  {
+    id: '1',
+    data: { label: 'Hello' },
+    position: { x: 0, y: 0 },
+    type: 'input',
+  },
+  {
+    id: '2',
+    data: { label: 'World' },
+    position: { x: 100, y: 100 },
+  },
+];
 
-const ModelCreatorView = ({ params }: Props) => {
-  const { modelId } = params;
+const initialEdges = [
+  { id: '1-2', source: '1', target: '2', label: 'to the', type: 'step' },
+];
+
+function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [],
+  );
 
   return (
-    <div>{modelId}</div>
-  )
+    <div style={{ height: 800 }}>
+      <ReactFlow
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        fitView
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
 
-export default ModelCreatorView
+export default Flow;
