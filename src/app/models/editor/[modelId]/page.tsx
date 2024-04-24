9dc2e68d6569c1f02d249cefc704d55f9d4f0612
dennisongaia <77359793+dennisongaia@users.noreply.github.com>
2024-04-24 12:07:04 -0700

    ParameterCell: Add test in model editor

--- src/app/models/editor/[modelId]/page.tsx
+++ src/app/models/editor/[modelId]/page.tsx
@@ -1,9 +1,17 @@
+"use client"
 import React from "react";
 import { ParameterList } from "../../_components/parameter-list";
 import { Canvas } from "../../_components/canvas";
 import { EditBar } from "../../_components/edit-bar-container";
 import { SearchBar } from "../../_components/search-bar";
 import { ParameterListSearch } from "../../_components/parameter-list-search";
+import DragIntoList from "../../_components/drag-into-list";
+
+import {defaultTheme, Provider} from '@adobe/react-spectrum';
+import { Flex, useListData } from "@adobe/react-spectrum";
+import DraggableListView from "../../_components/draggable-list-view";
+import DroppableListView from "../../_components/droppable-list-view";
+import { PARAMS } from "../../_constants/constants";
 
 type Props = {
   params: { modelId: string };
@@ -12,18 +20,33 @@ type Props = {
 const ModelCreatorView = ({ params }: Props) => {
   const { modelId } = params;
 
+  const sourceList = useListData({
+    initialItems: PARAMS
+  });
+
+  const targetList = useListData({
+    initialItems: [
+      
+    ],
+  });
+
   return (
+    <Provider theme={defaultTheme}  colorScheme="light">
     <div className="flex items-center justify-center align-middle">
       <div className="grid grid-rows-3 grid-flow-col">
         <div className="row-span-3 justify-left">
-          <ParameterListSearch/>
+          {/* <ParameterListSearch/> */}
+          <DraggableListView list={sourceList} />
+    
         </div>
         <div className="justify-right">
           <EditBar />
-          <Canvas />
+          <DroppableListView list={targetList} />
+          {/* <Canvas/> */}
         </div>
       </div>
     </div>
+    </Provider>
   );
 };
 
