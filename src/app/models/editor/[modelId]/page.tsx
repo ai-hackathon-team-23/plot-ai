31c0819e1ccc34d51f1efb7fa281e01ecfe050bb
dennisongaia <77359793+dennisongaia@users.noreply.github.com>
2024-04-25 10:34:31 -0700

    ParameterCell: Clean up edit page

--- src/app/models/editor/[modelId]/page.tsx
+++ src/app/models/editor/[modelId]/page.tsx
@@ -1,14 +1,11 @@
 "use client"
 import React from "react";
-import { ParameterList } from "../../_components/parameter-list";
 import { Canvas } from "../../_components/canvas";
 import { EditBar } from "../../_components/edit-bar-container";
 import { SearchBar } from "../../_components/search-bar";
-import { ParameterListSearch } from "../../_components/parameter-list-search";
-import DragIntoList from "../../_components/drag-into-list";
 
 import {defaultTheme, Provider} from '@adobe/react-spectrum';
-import { Flex, useListData } from "@adobe/react-spectrum";
+import { useListData } from "@adobe/react-spectrum";
 import DraggableListView from "../../_components/draggable-list-view";
 import { PARAMS } from "../../_constants/constants";
 
@@ -23,15 +20,12 @@ const ModelCreatorView = ({ params }: Props) => {
     initialItems: PARAMS
   });
 
-
   return (
     <Provider theme={defaultTheme}  colorScheme="light">
     <div className="flex items-center justify-center align-middle">
       <div className="grid grid-rows-3 grid-flow-col">
         <div className="row-span-3 justify-left">
-          {/* <ParameterListSearch/> */}
           <DraggableListView list={sourceList} />
-    
         </div>
         <div className="justify-right">
           <EditBar />
