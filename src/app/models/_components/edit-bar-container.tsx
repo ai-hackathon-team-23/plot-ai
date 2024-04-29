05344967d912c342847c37c916519d5625da6af0
dennisongaia <dennisongaia@gmail.com>
2024-04-28 21:01:12 -0700

    EditBar: Implement EditBar

--- src/app/models/_components/edit-bar-container.tsx
+++ src/app/models/_components/edit-bar-container.tsx
@@ -5,59 +5,60 @@ import { EDIT_OPTIONS, OPERATIONS } from "../_constants/constants";
 
 import { Checkbox } from "@/components/ui/checkbox";
 import { DropdownSelect } from "./dropdown-select";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "~/components/ui/input";
+import { useModelNodesContext } from "~/app/_context/model-context";
+import { createDynamicallyTrackedSearchParams } from "next/dist/client/components/search-params";
+import { get } from "node:https";
 
-interface EditBarProps {
-  blockTitle: string;
-  cellTitle: string;
-  blockAction: string;
-  setBlockAction: React.Dispatch<React.SetStateAction<string>>;
-  cellValue: string;
-  setCellValue: React.Dispatch<React.SetStateAction<string>>;
-  cellOperator: string;
-  setCellOperator: React.Dispatch<React.SetStateAction<string>>;
-  cellVisible: boolean;
-  setCellVisible: React.Dispatch<React.SetStateAction<boolean>>;
-}
+export function EditBar({}) {
+  const {focus, setUpdatedItem} = useModelNodesContext()
 
-export function EditBar({
-  blockTitle,
-  cellTitle,
-  blockAction,
-  setBlockAction,
-  cellValue,
-  setCellValue,
-  cellOperator,
-  setCellOperator,
-  cellVisible,
-  setCellVisible,
-}: EditBarProps) {
+  const [cellValue, setCellValue] = useState("");
+  const [cellOperator, setCellOperator] = useState("");
+  const [cellVisible, setCellVisible] = useState(true);
+
+  const handleOperatorChange = (value: string) => {
+    setCellOperator(value);
+  };
+
+  const handleSetVisible = (cellVisible: boolean) => {
+    setCellVisible(cellVisible);
+  };
+
+  useEffect(() => {
+    if(focus) {
+      setCellVisible(focus.visible);
+      setCellValue(focus.input);
+      setCellOperator(focus.operator);
+    }
+
+  }, [focus])
+
+
+  useEffect(() => {
+    if (focus) {
+      const newItem = {
+        id: focus.id,
+        section: focus.section,
+        value: focus.value,
+        label: focus.label,
+        format: focus.format,
+        input:  cellValue,
+        operator: cellOperator,
+        visible: cellVisible,
+      }
+      setUpdatedItem(newItem);
+    }
+
+  }, [cellValue, cellOperator, cellVisible]);
+
   return (
     <>
-      <div className="width-4 grid grid-cols-10 items-center justify-center border">
-        <div className="col-span-8">
-          {/* {Block Title Here} */}
-          <span className="inline-block pl-4 align-middle font-bold">
-            {blockTitle}
-          </span>
-        </div>
-        <div className="col-span-2 flex justify-end">
-          <span className="">
-            <DropdownSelect
-              size={"w-[200px]"}
-              placeholder={"Edit"}
-              items={EDIT_OPTIONS}
-              className="rounded-none border-0 border-l shadow-none"
-              onValueChange={setBlockAction}
-            />
-          </span>
-        </div>
-      </div>
       <div className="grid grid-cols-10 items-center justify-center border-b border-l">
         <div className="col-span-2 border-r">
           <span className="inline-block pl-4 align-middle font-bold">
-            {cellTitle}
+            {focus ? focus.label : "Cell Title"}
           </span>
         </div>
         <div className="col-span-6">
@@ -66,7 +67,8 @@ export function EditBar({
               type="number"
               placeholder="Cell Value"
               className="w-full border-0 focus-visible:ring-0"
-              onChange={(e) => setCellValue(e.target.value)}
+              value={cellValue}
+              onChange={e => setCellValue(e.target.value)}
             />
           </span>
         </div>
@@ -74,16 +76,18 @@ export function EditBar({
           <span className="pl-1">
             <DropdownSelect
               size={"w-[165px]"}
-              placeholder={"Operation"}
+              placeholder="Operation"
               items={OPERATIONS}
-              className="rounded-none border-0 border-x shadow-none"
+              value={cellOperator}
+              className="rounded-none border-0 border-x shadow-none" 
               onValueChange={setCellOperator}
             />
           </span>
           <span className="border-r pl-3 pr-2 pt-2">
             <Checkbox
               checked={cellVisible}
-              onCheckedChange={() => setCellVisible(cellVisible)}
+              value={cellVisible}
+              onCheckedChange={handleSetVisible}
               className={"border border-dashed"}
             />
           </span>
