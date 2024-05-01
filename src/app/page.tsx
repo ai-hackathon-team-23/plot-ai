394a9345021fadd56334a54a237569d3686d1df0
eth9n-dev <eckert6@gmail.com>
2024-05-01 13:13:12 -0700

    Redirect to login page based on session status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,28 +8,53 @@ export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await getServerAuthSession();
 
-  return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="flex flex-col justify-center pb-10">
-          <h1 className="text-4xl font-bold">Welcome to</h1>
-          <h1 className="text-8xl font-bold">plot-ai</h1>
-        </div>
-        <div className="flex items-center justify-center">
-          <div className="pr-2">
-            <Link href="/models" passHref>
-              <Button>Create a Model</Button>
-            </Link>
+  if (session != null) {
+    return (
+      <>
+        <main className="flex min-h-screen flex-col items-center justify-center">
+          <div className="flex flex-col justify-center pb-10">
+            <h1 className="text-4xl font-bold">Welcome to</h1>
+            <h1 className="text-8xl font-bold">plot-ai</h1>
           </div>
-          <div>
-            <Link href="/search" passHref>
-              <Button variant="outline">Start a Search</Button>
-            </Link>
+          <div className="flex items-center justify-center">
+            <div className="pr-2">
+              <Link href="/models" passHref>
+                <Button>Create a Model</Button>
+              </Link>
+            </div>
+            <div>
+              <Link href="/search" passHref>
+                <Button variant="outline">Start a Search</Button>
+              </Link>
+            </div>
           </div>
-        </div>
-      </main>
-    </>
-  );
+        </main>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <main className="flex min-h-screen flex-col items-center justify-center">
+          <div className="flex flex-col justify-center pb-10">
+            <h1 className="text-4xl font-bold">Welcome to</h1>
+            <h1 className="text-8xl font-bold">plot-ai</h1>
+          </div>
+          <div className="flex items-center justify-center">
+            <div className="pr-2">
+              <Link href="/api/auth/signin" passHref>
+                <Button>Log in to Begin!</Button>
+              </Link>
+            </div>
+            <div>
+              <Link href="/api/auth/signin" passHref>
+                <Button variant="outline">Start a Search</Button>
+              </Link>
+            </div>
+          </div>
+        </main>
+      </>
+    );
+  }
 }
 
 async function CrudShowcase() {
